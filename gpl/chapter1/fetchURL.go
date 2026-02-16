355dbffc6dc9bbd934373d7582610d452680e49c
Manish Sharma <manish.sharma0201cs@gmail.com>
2026-02-16 06:27:57 +0000

    Adding url fetch implementation with filename

--- gpl/chapter1/fetchURL.go
+++ gpl/chapter1/fetchURL.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
 		response, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error occured reading the URL %v", err)
 			os.Exit(1)
 		}
-		content, err := ioutil.ReadAll(response.Body)
+		content, err := io.Copy(os.Stdout, response.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error occured reading the content %v", err)
 			os.Exit(1)
 		}
 		fmt.Printf("URL contents are: %s", content)
+		fmt.Printf("HTTP status code: ", response.Status)
 	}
 }
